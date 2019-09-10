3f856a8d5e548e48833ba99786767134b47b27da
Kike Sanchez <kikin1601@hotmail.com>
2019-09-10 17:22:49 -0500

    Menu lateral

--- src/app/menulateral/menulateral.component.ts
+++ src/app/menulateral/menulateral.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle, faSearch, faListUl, faLayerGroup, faCode, faCheckSquare, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faAngular } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-menulateral',
@@ -9,6 +10,14 @@ import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 })
 export class MenulateralComponent implements OnInit {
   faUserCircle = faUserCircle
+  faSearch = faSearch
+  faListUl = faListUl
+  faLayerGroup = faLayerGroup
+  faCode = faCode
+  faCheckSquare = faCheckSquare
+  faHeart = faHeart
+  faAngular = faAngular
+
   busqueda:FormGroup
   constructor() { }
 
